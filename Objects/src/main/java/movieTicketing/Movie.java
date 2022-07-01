d4a710e3a7cb8a15f116582049f6b75ebca76c1e
joohyeongkim <wnguddl96@knou.ac.kr>
2022-07-01 21:55:35 +0900

    0701

--- Objects/src/main/java/movieTicketing/Movie.java
+++ Objects/src/main/java/movieTicketing/Movie.java
@@ -13,11 +13,12 @@ public class Movie {
     Movie avatar = new Movie("아바타",
             Duration.ofMinutes(120),
             Money.wons(10000),
-            new AmountDiscountPolicy(Money.wons(800),
-                    new SequenceCondition(1),
-                    new SequenceCondition(10),
-                    new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10,0), LocalTime.of(11,59)),
-                    new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10,0), LocalTime.of(20,59))));
+            new PercentDiscountPolicy(0.1));
+//            new AmountDiscountPolicy(Money.wons(800),
+//                    new SequenceCondition(1),
+//                    new SequenceCondition(10),
+//                    new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10,0), LocalTime.of(11,59)),
+//                    new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10,0), LocalTime.of(20,59))));
     Movie titanic = new Movie("타이타닉",
             Duration.ofMinutes(180),
             Money.wons(11000),
