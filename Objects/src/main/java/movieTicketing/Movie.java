52836b5992b5f7b97f0a6ac12d83e69b37dcb1e0
joohyeongkim <wnguddl96@knou.ac.kr>
2022-07-04 21:05:30 +0900

    0704 Movie should be changed all

--- Objects/src/main/java/movieTicketing/Movie.java
+++ Objects/src/main/java/movieTicketing/Movie.java
@@ -14,11 +14,7 @@ public class Movie {
             Duration.ofMinutes(120),
             Money.wons(10000),
             new PercentDiscountPolicy());
-//            new AmountDiscountPolicy(Money.wons(800),
-//                    new SequenceCondition(1),
-//                    new SequenceCondition(10),
-//                    new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10,0), LocalTime.of(11,59)),
-//                    new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10,0), LocalTime.of(20,59))));
+
     Movie titanic = new Movie("타이타닉",
             Duration.ofMinutes(180),
             Money.wons(11000),
